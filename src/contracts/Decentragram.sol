29ef875bf6bcab6a7783bde4dc9860566a05f340
Yaman Aggarwal <yaman2661@gmail.com>
2021-03-13 23:53:42 +0530

    first commit

---
+++ src/contracts/Decentragram.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.5.0;
+
+contract Decentragram {
+    string public name = "Decentragram";
+
+    struct Image {
+        uint256 id;
+        string hash; //location of IPFS
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    //Store Images
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    //Create Images
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        require(bytes(_imgHash).length > 0);
+        require(bytes(_description).length > 0);
+        require(msg.sender != address(0));
+
+        imageCount++;
+
+        //Add image to contract
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender
+        );
+
+        //trigger event
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    //Tip Images
+    function tipImageOwner(uint256 _id) public payable {
+        require(_id > 0 && _id <= imageCount);
+
+        Image memory _image = images[_id];
+
+        // Fetch the author
+        address payable _author = _image.author;
+
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+
+        // Increment the tip amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+
+        // Update the image
+        images[_id] = _image;
+
+        // Trigger an event
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
+}
